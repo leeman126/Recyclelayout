924d7f394fd57fd89c06009a7df44fee1459e9e7
Leeman <thiesno.diallo@sesame.com.tn>
2022-04-06 12:41:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recycler_layout/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.recycler_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+RecyclerView rv;
+String [] titre;
+String [] Soust;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv= findViewById(R.id.RecyclerV);
+
+        titre=getResources().getStringArray(R.array.tit);
+        Soust=getResources().getStringArray(R.array.tit);
+        ListAdapter ad;
+
+        Integer[] img ={R.drawable.ic_home,R.drawable.ic_search,R.drawable.ic_home,
+                R.drawable.ic_search,R.drawable.ic_home,R.drawable.ic_search};
+
+        ad = new ListAdapter(MainActivity.this,titre,Soust,img);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(ad);
+
+
+
+    }
+}
